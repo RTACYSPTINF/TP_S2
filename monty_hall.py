df785f605a63b560a387d6fb5e5f80d28586436c
jean-pierre SPITONI <jpspitoni@gmail.com>
2020-04-09 23:29:03 +0200

    send cgt

--- monty_hall.py
+++ monty_hall.py
@@ -1,5 +1,6 @@
 import random
 
+
 # utiliser cette classe permet de rendre le programme plus lisible
 class Strategie:
     def __init__(self):
@@ -41,7 +42,7 @@ def jeux_unique(strategie):
 strat=Strategie()
 
 
-def monty_hall_simu1(stat, nb_emissions):
+def monty_hall_simu1(strat, nb_emissions):
     """
 
     :param stategie:
@@ -52,7 +53,7 @@ def monty_hall_simu1(stat, nb_emissions):
     return [1 if jeux_unique(strat)==1 else 0 for i in range(nb_emissions)]
 
 
-def monty_hall_simu2(stat, nb_emissions):
+def monty_hall_simu2(strat, nb_emissions):
     """
 
     :param stategie:
@@ -65,3 +66,25 @@ def monty_hall_simu2(stat, nb_emissions):
     return liste_final
 
 
+
+def main():
+    strat=Strategie()  # on crée une instance de la classe Strategie
+    nb_emission=100000
+    # strategie je ne change pas (je garde la porte de mon premier choix)
+    premier_choix=monty_hall_simu1(strat.garder, nb_emission)
+    # Strategie je change
+    second_choix=monty_hall_simu1(strat.changer,nb_emission)
+    print("#############################################################")
+    print("#############################################################")
+    print(f" en conservant mon premier choix  (Stratégie = garder), je gagne {sum(premier_choix)} sur un toltal de {nb_emission}")
+    print("----****************************************************-----")
+    print(f" en changeant mon premier choix  (Stratégie = changer), je gagne {sum(second_choix)} sur un toltal de {nb_emission}")
+    print("#############################################################")
+    print("#############################################################")
+    print(f" en conservant mon premier choix  (Stratégie = garder), je gagne {sum(premier_choix)/nb_emission*100:.2f} %, sur un toltal de {nb_emission}")
+    print("----****************************************************-----")
+    print(f" en changeant mon premier choix  (Stratégie = changer), je gagne {sum(second_choix)/nb_emission*100:.2f} %, sur un toltal de {nb_emission}")
+
+
+if __name__=="__main__":
+    main()
